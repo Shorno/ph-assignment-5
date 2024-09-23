46566d5bd209c405ad98188957b3e98b9f8aae61
Shorno <roy15-6141@s.diu.edu.bd>
2024-09-23 16:44:20 +0600

    refactoring js

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
-const donationBtn = document.getElementById("donationBtn");
-const historyBtn = document.getElementById("historyBtn");
-let receivedDonationAmount = parseFloat(document.getElementById("receivedDonationAmount").innerText);
-const donateBtn = document.getElementById("donateBtn");
-let mainBalance = parseFloat(document.getElementById("mainBalance").innerText);
+const getElement = (id) => document.getElementById(id);
+const parseFloatFromElement = (id) => parseFloat(getElement(id).innerText);
+const updateElementText = (id, value) => getElement(id).innerText = value.toFixed(2);
+const getInputValue = (id) => getElement(id).value;
 
+let mainBalance = parseFloatFromElement("mainBalance");
+let receivedDonationAmount = parseFloatFromElement("receivedDonationAmount");
 
-function toggleButton(activeBtn, inactiveBtn) {
+const toggleButton = (activeBtn, inactiveBtn) => {
     activeBtn.classList.remove("bg-base-200", "text-gray-500");
     activeBtn.classList.add("bg-lime-400", "text-black");
     inactiveBtn.classList.remove("bg-lime-400", "text-black");
     inactiveBtn.classList.add("bg-base-200", "text-gray-500");
-}
+};
 
-historyBtn.addEventListener("click", () => {
-    toggleButton(historyBtn, donationBtn);
-})
+const handleDonation = (inputId, receivedAmountId) => {
+    const donationAmountString = getInputValue(inputId)
+    if (donationAmountString === "" || isNaN(donationAmountString)) {
+        alert("Please enter a valid amount");
+        return;
+    }
 
-donationBtn.addEventListener("click", () => {
-    toggleButton(donationBtn, historyBtn);
-})
-
-donateBtn.addEventListener("click", () => {
-    makeDonation();
-})
-
-const makeDonation = () => {
-    const donationInputAmount = parseFloat(document.getElementById("donationInputAmount").value);
-    const modal = document.getElementById("my_modal_5");
+    const donationInputAmount = parseFloat(getElement(inputId).value);
+    const modal = getElement("my_modal_5");
 
     if (donationInputAmount > 0 && donationInputAmount <= mainBalance) {
         receivedDonationAmount += donationInputAmount;
         mainBalance -= donationInputAmount;
-        document.getElementById("mainBalance").innerText = mainBalance.toFixed(2);
-        document.getElementById("receivedDonationAmount").innerText = receivedDonationAmount.toFixed(2);
+        updateElementText("mainBalance", mainBalance);
+        updateElementText(receivedAmountId, receivedDonationAmount);
         modal.showModal();
     } else {
         alert("Insufficient balance");
     }
-}
+};
 
 
+const donationBtn = getElement("donationBtn");
+const historyBtn = getElement("historyBtn");
+const donateBtn = getElement("donateBtn");
 
+historyBtn.addEventListener("click", () => {
+    toggleButton(historyBtn, donationBtn);
+});
+
+donationBtn.addEventListener("click", () => {
+    toggleButton(donationBtn, historyBtn);
+});
+
+donateBtn.addEventListener("click", () => {
+    handleDonation("donationInputAmount", "receivedDonationAmount");
+});
